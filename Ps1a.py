df709b9fd91f63b5db64825dadf3ee3b1e062d5a
Hexpee <52126266+Hexpee@users.noreply.github.com>
2019-09-15 00:46:59 +0100

    Add files via upload

---
+++ Ps1a.py
@@ -0,0 +1,15 @@
+#A program that calculate month it take a user to buy house in Bay Area
+annualSalary = float(input("Enter your annual salary: "))
+#portion saved
+portionSaved = float(input("Enter the percent of your salary to save, in decimal: "))
+totalCost = float(input("Enter the cost of your dream home: "))
+#to get the portion down payment 
+portionDownPayment = totalCost * 0.25
+#initializing the current saving getting a return value from monthly salary X by portion saved plus value of current+
+currentSavings = 0
+r = 0.04
+months = 0
+while currentSavings < portionDownPayment:
+    currentSavings += (annualSalary/12)*portionSaved + currentSavings*(r/12)
+    months += 1
+print("Number of months to saved: ", months)
